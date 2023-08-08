f9ddb7013de25cd184192820f97e301c9817e349
arturwyroslak <95839215+arturwyroslak@users.noreply.github.com>
2023-08-08 11:43:46 +0200

    Update PluginManagmentDialog.js

--- app/components/PluginManagmentDialog.js
+++ app/components/PluginManagmentDialog.js
@@ -3,51 +3,62 @@ import TextField from "./TextField";
 import { useState, useEffect } from "react";
 import { useLiveQuery } from "dexie-react-hooks";
 
-const DEFAULT_PLUGINS_SERVER = "https://raw.githubusercontent.com/masasron/zik-plugins/main/plugins.json";
+const DEFAULT_PLUGINS_SERVER = "https://sed2.vercel.app/plugins.json";
+const ITEMS_PER_PAGE = 10;
 
 function Plugin(props) {
     const plugin = props.plugin;
-    return <div index={props.index} className="plugin">
-        <div className="plugin-header">
-            <div className="logo">
-                <img src={plugin.logo_url} />
+    return (
+        <div index={props.index} className="plugin">
+            <div className="plugin-header">
+                <div className="logo">
+                    <img src={plugin.logo_url} alt={plugin.name_for_human} />
+                </div>
+                <div className="content">
+                    <h3>{plugin.name_for_human}</h3>
+                    {plugin.installed ? (
+                        <button
+                            className="primary gray"
+                            onClick={() => uninstallPlugin(plugin.name_for_human)}
+                        >
+                            Uninstall
+                            <span className="material-symbols-outlined">cancel</span>
+                        </button>
+                    ) : (
+                        <button className="primary" onClick={() => installPlugin(plugin)}>
+                            Install Plugin
+                            <span className="material-symbols-outlined">download</span>
+                        </button>
+                    )}
+                </div>
             </div>
-            <div className="content">
-                <h3>
-                    {plugin.name_for_human}
-                </h3>
-                {plugin.installed && <button className="primary gray" onClick={() => uninstallPlugin(plugin.name_for_human)}>
-                    Uninstall
-                    <span className="material-symbols-outlined">
-                        cancel
-                    </span>
-                </button>}
-                {!plugin.installed && <button className="primary" onClick={() => installPlugin(plugin)}>
-                    Install Plugin
-                    <span className="material-symbols-outlined">
-                        download
-                    </span>
-                </button>}
+            <p>{plugin.description_for_human}</p>
+            <div>
+                {plugin.legal_info_url && (
+                    <a
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        href={
+                            typeof plugin.legal_info_url === "string" &&
+                            plugin.legal_info_url.match(/^https?:\/\//i) &&
+                            plugin.legal_info_url
+                        }
+                    >
+                        <span className="material-symbols-outlined">gavel</span>
+                    </a>
+                )}
+                {plugin.contact_email && (
+                    <a href={"mailto:" + plugin.contact_email}>
+                        <span className="material-symbols-outlined">mail</span>
+                    </a>
+                )}
             </div>
         </div>
-        <p>{plugin.description_for_human}</p>
-        <div>
-            {plugin.legal_info_url && <a target="_blank" rel="noreferrer noopener" href={typeof plugin.legal_info_url === "string" && plugin.legal_info_url.match(/^https?:\/\//i) && plugin.legal_info_url}>
-                <span className="material-symbols-outlined">
-                    gavel
-                </span>
-            </a>}
-            {plugin.contact_email && <a href={"mailto:" + plugin.contact_email}>
-                <span className="material-symbols-outlined">
-                    mail
-                </span>
-            </a>}
-        </div>
-    </div>
+    );
 }
 
 async function uninstallPlugin(name) {
-    if (confirm(`Are you sure you want to uninstall "${name}"?`)) {
+    if (window.confirm(`Are you sure you want to uninstall "${name}"?`)) {
         await db.plugins.where("name_for_human").equals(name).delete();
     }
 }
@@ -58,7 +69,7 @@ async function installPlugin(plugin) {
         warning += "\nPlease note, this plugin requires a TODO server to run locally.";
     }
 
-    if (!confirm(warning)) {
+    if (!window.confirm(warning)) {
         return;
     }
 
@@ -136,79 +147,136 @@ function AddPluginFromURLDialog(props) {
         }
     }
 
-    return <>
-        <header>
-            <span onClick={props.onClose} className="material-symbols-outlined">close</span>
-            <h1>
-                <button style={{ marginRight: 10 }} onClick={() => props.updatePage(0)}>Back</button>
-                Add plugin
-            </h1>
-        </header>
-        <div className="dialog-content">
-            <form onSubmit={handleInstallation}>
-                <TextField label="URL" hint={error} onChange={downloadPlugin} placeholder="https://example.com" type="url" pattern="^https?://.*" title="Must be a valid website URL" />
-                {plugin && <div style={{ display: "flex" }}>
-                    <Plugin plugin={plugin} />
-                </div>}
-            </form>
-        </div>
-    </>
+    return (
+        <>
+            <header>
+                <span onClick={props.onClose} className="material-symbols-outlined">
+                    close
+                </span>
+                <h1>
+                    <button style={{ marginRight: 10 }} onClick={() => props.updatePage(0)}>
+                        Back
+                    </button>
+                    Add plugin
+                </h1>
+            </header>
+            <div className="dialog-content">
+                <form onSubmit={handleInstallation}>
+                    <TextField
+                        label="URL"
+                        hint={error}
+                        onChange={downloadPlugin}
+                        placeholder="https://example.com"
+                        type="url"
+                        pattern="^https?://.*"
+                        title="Must be a valid website URL"
+                    />
+                    {plugin && (
+                        <div style={{ display: "flex" }}>
+                            <Plugin plugin={plugin} />
+                        </div>
+                    )}
+                </form>
+            </div>
+        </>
+    );
 }
 
 function InstalledPluginsList(props) {
     const [plugins, setPlugins] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [currentPage, setCurrentPage] = useState(1);
     const installedPlugins = useLiveQuery(() => db.plugins.toArray());
 
-    useEffect(function () {
+    useEffect(() => {
         const plugins_server_url = props?.settings?.plugins_server_url || DEFAULT_PLUGINS_SERVER;
-        fetch(plugins_server_url + "?r=" + Math.random(), { cache: "no-cache" }).then(response => response.json()).then(p => {
-            // Add "installed" property to each plugin
-            p = p.map(p => {
-                p.installed = installedPlugins?.find(({ name_for_human }) => name_for_human === p.name_for_human) ? true : false;
-                return p;
+        fetch(plugins_server_url + "?r=" + Math.random(), { cache: "no-cache" })
+            .then((response) => response.json())
+            .then((p) => {
+                // Add "installed" property to each plugin
+                p = p.map((p) => {
+                    p.installed = installedPlugins?.find(({ name_for_human }) => name_for_human === p.name_for_human)
+                        ? true
+                        : false;
+                    return p;
+                });
+
+                // Add installed plugins to the p array if they're not already there
+                installedPlugins?.forEach((plugin) => {
+                    if (!p.find(({ name }) => name === plugin.name)) {
+                        plugin.installed = true;
+                        p.push(plugin);
+                    }
+                });
+
+                setPlugins(p);
+            })
+            .catch((error) => {
+                alert("Failed to fetch plugins list, due to: " + error);
             });
+    }, [installedPlugins, props?.settings?.plugins_server_url]);
 
-            // Add installed plugins to the p array if they're not already there
-            installedPlugins?.forEach(plugin => {
-                if (!p.find(({ name }) => name === plugin.name)) {
-                    plugin.installed = true;
-                    p.push(plugin);
-                }
-            });
+    const filteredPlugins = plugins.filter((plugin) =>
+        plugin.name_for_human.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-            setPlugins(p);
-        }).catch(error => {
-            alert("Failed to fetch plugins list, due to: " + error);
-        });
-    }, [installedPlugins]);
+    const totalPages = Math.ceil(filteredPlugins.length / ITEMS_PER_PAGE);
 
-    return <>
-        <header>
-            <span onClick={props.onClose} className="material-symbols-outlined">close</span>
-            <h1>
-                Plugins
-                <button style={{ marginLeft: 10 }} onClick={() => props.updatePage(1)}>Add from URL</button>
-            </h1>
-        </header>
-        <div className="dialog-content">
-            <div className="plugins">
-                {plugins && plugins.map((plugin, index) => <Plugin plugin={plugin} index={index} />)}
+    const currentPlugins = filteredPlugins.slice(
+        (currentPage - 1) * ITEMS_PER_PAGE,
+        currentPage * ITEMS_PER_PAGE
+    );
+
+    return (
+        <>
+            <header>
+                <span onClick={props.onClose} className="material-symbols-outlined">
+                    close
+                </span>
+                <h1>
+                    Plugins
+                    <button style={{ marginLeft: 10 }} onClick={() => props.updatePage(1)}>
+                        Add from URL
+                    </button>
+                </h1>
+            </header>
+            <div className="dialog-content">
+                <TextField
+                    label="Search"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <div className="plugins">
+                    {currentPlugins && currentPlugins.map((plugin, index) => <Plugin key={index} plugin={plugin} />)}
+                </div>
+                <div>
+                    <button onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}>Previous</button>
+                    <span>
+                        {currentPage} / {totalPages}
+                    </span>
+                    <button onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}>Next</button>
+                </div>
+                <hr />
+                <small>
+                    When using plugins, be aware that a lengthy system prompt may be sent. Please take the time to
+                    thoroughly read and understand the plugin code prior to use.
+                </small>
             </div>
-            <hr />
-            <small>When using plugins, be aware that a lengthy system prompt may be sent. Please take the time to thoroughly read and understand the plugin code prior to use.</small>
-        </div>
-    </>
+        </>
+    );
 }
 
 export default function PluginManagmentDialog(props) {
     const [page, setPage] = useState(0);
 
-    return <>
+    return (
         <div className="dialog-container">
             <div className="dialog">
-                {page === 0 && <InstalledPluginsList settings={props.settings} onClose={props.onClose} updatePage={setPage} />}
+                {page === 0 && (
+                    <InstalledPluginsList settings={props.settings} onClose={props.onClose} updatePage={setPage} />
+                )}
                 {page === 1 && <AddPluginFromURLDialog onClose={props.onClose} updatePage={setPage} />}
             </div>
         </div>
-    </>
+    );
 }
